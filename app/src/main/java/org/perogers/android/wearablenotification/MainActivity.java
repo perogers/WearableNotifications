1cf73f4f022b126005c8ec3aaef9097e3edaba99
paulrogers <rogers.paul.work@gmail.com>
2016-05-31 08:08:22 -0400

    WIP - Added result activity

--- app/src/main/java/org/perogers/android/wearablenotification/MainActivity.java
+++ app/src/main/java/org/perogers/android/wearablenotification/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,6 +18,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    // Key for the string that's delivered in the action's intent
+    private static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +49,17 @@ public class MainActivity extends AppCompatActivity {
         int notificationId = 001;
 
         // Build intent for notification content
-        Intent viewIntent = new Intent(this, MainActivity.class);
+        Intent resultIntent = new Intent(this, ResultActivity.class);
 
-        PendingIntent viewPendingIntent =
-                PendingIntent.getActivity(this, 0, viewIntent, 0);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(this, 0, resultIntent, 0);
 
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_notifications_white_18dp)
                         .setContentTitle( getString(R.string.notification_title) )
                         .setContentText( getString(R.string.notification_content) )
-                        .setContentIntent(viewPendingIntent);
+                        .setContentIntent(resultPendingIntent);
 
         // Get an instance of the NotificationManager service
         NotificationManagerCompat notificationManager =
@@ -65,9 +68,32 @@ public class MainActivity extends AppCompatActivity {
         // Build the notification and issues it with notification manager.
         notificationManager.notify(notificationId, notificationBuilder.build());
 
+        // Voice response
+    /*
+        String replyLabel = getResources().getString(R.string.reply_label);
+
+        RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
+                .setLabel(replyLabel)
+                .build();
+*/
         Log.i(TAG, "Notification sent");
 
 
+    }
+
+
+
+    /**
+     * Obtain the intent that started this activity by calling
+     * Activity.getIntent() and pass it into this method to
+     * get the associated voice input string.
+     */
+    private CharSequence getMessageText(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(EXTRA_VOICE_REPLY);
+        }
+        return null;
     }
 
 
